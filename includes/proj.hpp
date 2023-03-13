0a6526f1494977e4c85050af15e168145c8fc021
Jefferson Davis <davisjd.jefferson23@gmail.com>
2023-03-13 12:23:01 +0200

    Cleaning up. Next have program read shader compilation string from file

--- includes/proj.hpp
+++ includes/proj.hpp
@@ -7,20 +7,41 @@
 #include "Shader.hpp"
 #include "ProgShader.hpp"
 #include <iostream>
+#include <cmath>
 
 static const char *vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0";
+"layout (location = 0) in vec3 aPos;\n" // the position variable has attribute position 0
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n" // specify a color output to the fragment shader
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos, 1.0);\n" // see how we directly give a vec3 to vec4's constructor
+"    ourColor = aColor;\n" // set the output variable to a dark-red color
+"}\0";
 
 static const char *fragShaderSource = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"void main()\n"
-	"{\n"
-	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-	"}\0";
+"out vec4 FragColor;\n" 
+"in vec3 ourColor;\n" // the input variable from the vertex shader (same name and same type)  
+"void main()\n"
+"{\n"
+"    FragColor = vec4(ourColor, 1.0);\n"
+"}\0";
+
+
+
+// static const char *vertexShaderSource = "#version 330 core\n"
+// 	"layout (location = 0) in vec3 aPos;\n"
+// 	"void main()\n"
+// 	"{\n"
+// 	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+// 	"}\0";
+
+// static const char *fragShaderSource = "#version 330 core\n"
+// 	"out vec4 FragColor;\n"
+// 	"void main()\n"
+// 	"{\n"
+// 	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+// 	"}\0";
 
 
 #endif
